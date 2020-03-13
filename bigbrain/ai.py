286f5b1ea6796778bac908001fbee3f71867c8b3
Gavin Mischler <gavinmischler@gmail.com>
2020-03-13 16:35:28 -0400

    added poly svm kernel

--- bigbrain/ai.py
+++ bigbrain/ai.py
@@ -53,7 +53,9 @@ class AI():
         if self.model_type_ == "classification":
             models = [KNeighborsClassifier(3),
                 SVC(kernel="linear", C=0.025),
-                SVC(gamma=2, C=1),GaussianProcessClassifier(1.0 * RBF(1.0)),
+                SVC(gamma=2, C=1),
+                SVC(kernel="poly", C=1),
+                GaussianProcessClassifier(1.0 * RBF(1.0)),
                 DecisionTreeClassifier(max_depth=5),
                 RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1), 
                 MLPClassifier(alpha=1, max_iter=1000),
