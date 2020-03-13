8119a630722df35587f02cd1badf1626ae829ff3
Joanna Guo <guo.joanna@hotmail.com>
2020-03-13 17:57:34 -0400

    added regression option

--- bigbrain/ai.py
+++ bigbrain/ai.py
@@ -1,16 +1,33 @@
 # Artificial Intelligence is finally here
 
+# data modules
 from sklearn.model_selection import train_test_split
+
+# kernel modules
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, RationalQuadratic
+
+# classification modules
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.metrics import accuracy_score
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+
+# regression modules
+from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.linear_model import Lasso
+
+# evaluation modules
+from sklearn.metrics import accuracy_score, mean_squared_error
+from numpy import inf
 from sklearn.model_selection import GridSearchCV
 
 
@@ -51,40 +68,74 @@ class AI():
 
         # loop through list of supervised learning classification methods
         if self.model_type_ == "classification":
-            models = [KNeighborsClassifier(3),
+            models = [MLPClassifier(alpha=1, max_iter=1000),
+                KNeighborsClassifier(),
                 SVC(kernel="linear", C=0.025),
-                SVC(gamma=2, C=1),
                 SVC(kernel="poly", C=1),
-                GaussianProcessClassifier(1.0 * RBF(1.0)),
+                SVC(kernel="rbf", gamma=2, C=1),
+                SVC(kernel="sigmoid", C=1),
+                GaussianProcessClassifier(RBF()),
+                GaussianProcessClassifier(ConstantKernel()),
+                GaussianProcessClassifier(RationalQuadratic()),
                 DecisionTreeClassifier(max_depth=5),
-                RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1), 
-                MLPClassifier(alpha=1, max_iter=1000),
+                RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
                 AdaBoostClassifier(),
                 GaussianNB(),
+                LinearDiscriminantAnalysis(),
                 QuadraticDiscriminantAnalysis()]
 
+            best_score = 0
+
+        else:
+            models = [MLPRegressor(alpha=1, max_iter=1000),
+                KNeighborsRegressor(),
+                SVR(kernel="linear", C=0.025),
+                SVR(kernel="poly", C=1),
+                SVR(kernel="rbf", gamma=2, C=1),
+                SVR(kernel="sigmoid", C=1),
+                GaussianProcessRegressor(RBF()),
+                GaussianProcessRegressor(ConstantKernel()),
+                GaussianProcessRegressor(RationalQuadratic()),
+                DecisionTreeRegressor(max_depth=5),
+                RandomForestRegressor(max_depth=5, n_estimators=10, max_features=1),
+                AdaBoostRegressor(),
+                Lasso()]
+
+            best_score = inf
+
         # for each model, fit on training data then predict on testing data
-        best_score = 0
 
         for m in models:
             m.fit(X_train, y_train)
             y_pred = m.predict(X_test)
-            score = accuracy_score(y_test, y_pred)
 
-            # if testing accuracy is the best so far
-            if score > best_score:
-                # set it as the best score and best model
-                best_score = score
-                self.model_ = m
+            if self.model_type_ == "classification":
+                score = accuracy_score(y_test, y_pred)
+
+                # if testing accuracy is the best so far
+                if score > best_score:
+                    # set it as the best score and best model
+                    best_score = score
+                    self.model_ = m
+                score_name = "accuracy score"
+            else:
+                score = mean_squared_error(y_test, y_pred)
+
+                # if testing accuracy is the best so far
+                if score < best_score:
+                    # set it as the best score and best model
+                    best_score = score
+                    self.model_ = m
+                score_name = "MSE"
 
         # grid search parameterss for best model and save
 
         # done learning
-        print("My big brain has learned everything.")
+        print("My big brain has learned everything.\n")
 
         if self.verbose_:
             print("Best model: %s" % self.model_)
-            print("Best model score: %0.3f" % best_score)
+            print("Best %s: %0.3f" % (score_name, best_score))
 
     def go(self, X):
         """
